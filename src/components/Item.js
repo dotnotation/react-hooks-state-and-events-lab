bd432d3f11ac49cd3bb72ff7e9755c33e92566cb
dotnotation <dotinmotionproductions@gmail.com>
2022-04-20 14:28:45 -0400

    completed deliverables

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import {useState} from "react";
 
 function Item({ name, category }) {
+  const [cartItem, setCartItem] = useState("")
+  
+  function handleClick(){
+    cartItem === "" ? setCartItem("in-cart") : setCartItem("")
+  }
+  
   return (
-    <li className="">
+    <li className={cartItem}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{cartItem === "" ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
